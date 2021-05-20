4129e2d973a85340a2f08901c4bd45a7d2f1644c
Georgii Vlasov <honest.niceman@gmail.com>
2021-05-21 01:32:16 +0400

    add plugins

--- modules/core/test/com/company/untitled1/Untitled1TestContainer.java
+++ modules/core/test/com/company/untitled1/Untitled1TestContainer.java
@@ -11,10 +11,10 @@ public class Untitled1TestContainer extends TestContainer {
     public Untitled1TestContainer() {
         super();
         //noinspection ArraysAsListWithZeroOrOneArgument
-        appComponents = new ArrayList<>(Arrays.asList(
-                // list add-ons here: "com.haulmont.reports", "com.haulmont.addon.bproc", etc.
-                "com.haulmont.cuba"
-        ));
+        appComponents = Arrays.asList(
+                "com.haulmont.cuba",
+                "com.haulmont.charts",
+                "com.haulmont.addon.helium");
         appPropertiesFiles = Arrays.asList(
                 // List the files defined in your web.xml
                 // in appPropertiesConfig context parameter of the core module
--- modules/web/test/com/company/untitled1/Untitled1WebTestContainer.java
+++ modules/web/test/com/company/untitled1/Untitled1WebTestContainer.java
@@ -8,10 +8,10 @@ import java.util.Arrays;
 public class Untitled1WebTestContainer extends TestContainer {
 
     public Untitled1WebTestContainer() {
-        appComponents = new ArrayList<>(Arrays.asList(
-                "com.haulmont.cuba"
-                // add CUBA add-ons and custom app components here
-        ));
+        appComponents = Arrays.asList(
+                "com.haulmont.cuba",
+                "com.haulmont.charts",
+                "com.haulmont.addon.helium");
         appPropertiesFiles = Arrays.asList(
                 // List the files defined in your web.xml
                 // in appPropertiesConfig context parameter of the web module
